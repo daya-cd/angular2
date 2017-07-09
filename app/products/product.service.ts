3e6d847b31af0ba257d8602fa0e5ffce2340b897
Dayanithi Hari <h.dayanithi@gmail.com>
2017-07-09 17:06:16 +0100

    added ng modules for github pagea

--- app/products/product.service.ts
+++ app/products/product.service.ts
@@ -6,7 +6,7 @@ import 'rxjs/add/operator/do';
 import 'rxjs/add/operator/catch';
 import 'rxjs/add/operator/map';
 
-import { IProduct } from './product';
+import { IProduct } from './products';
 
 @Injectable()
 export class ProductService {
